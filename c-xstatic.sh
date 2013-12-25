649f6766b4bce37c7685ed908f3850834c159d49
Peter Szabo <pts@fazekas.hu>
2013-12-25 23:21:37 +0100

    added stripping to save 136 bytes

--- c-xstatic.sh
+++ c-xstatic.sh
@@ -1,4 +1,7 @@
 #! /bin/sh --
+#
+# See the explanation why these flags are useful for small output here:
+# http://ptspts.blogspot.hu/2013/12/how-to-make-smaller-c-and-c-binaries.html
 
 set -ex
 # The warning ``main: warning: the use of LEGACY `utimes' is discouraged,
@@ -9,6 +12,11 @@ xstatic gcc -m32 -s -Os -W -Wall -o tiny7zx \
     7zAlloc.c 7zBuf.c 7zBuf2.c 7zCrc.c 7zCrcOpt.c 7zDec.c 7zFile.c 7zIn.c \
     7zMain.c 7zStream.c Bcj2.c Bra.c Bra86.c CpuArch.c Lzma2Dec.c LzmaDec.c \
     Ppmd7.c Ppmd7Dec.c
+# This stripping saves about 136 bytes.
+strip -s --remove-section=.note.gnu.gold-version \
+    --remove-section=.comment --remove-section=.note \
+    --remove-section=.note.gnu.build-id \
+    tiny7zx
 upx.pts --ultra-brute tiny7zx
 
 : c-xstatic.sh OK.
