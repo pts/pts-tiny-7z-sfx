fabf485359c88ff54b7688fa9298783d31367abc
Peter Szabo <pts@fazekas.hu>
2013-12-25 17:50:32 +0100

    fixed #include directories

--- 7zMain.c
+++ 7zMain.c
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "../../7z.h"
-#include "../../7zAlloc.h"
-#include "../../7zCrc.h"
-#include "../../7zFile.h"
-#include "../../7zVersion.h"
+#include "7z.h"
+#include "7zAlloc.h"
+#include "7zCrc.h"
+#include "7zFile.h"
+#include "7zVersion.h"
 
 #ifndef USE_WINDOWS_FILE
 /* for mkdir */
