b73d0adb3503fc838652a5475f6817a9c5393978
Peter Szabo <pts@fazekas.hu>
2014-10-22 02:45:29 +0200

    added xtiny.scr linker script (to get the full benefits, .o files in .a files had to be stripped first from .eh_frame)

--- c-minidiet.sh
+++ c-minidiet.sh
@@ -6,7 +6,6 @@
 # See the explanation why these flags are useful for small output here:
 # http://ptspts.blogspot.hu/2013/12/how-to-make-smaller-c-and-c-binaries.html
 #
-# TODO(pts): Try to use a linker script from xtiny. Can it initialize dietlibc?
 # TODO(pts): Disable WANT_STACKGAP for dietlibc.
 # TODO(pts): -D_FILE_OFFSET_BITS=64, fopen64 etc.
 # TODO(pts): Use fread_unlocked, *_unlocked etc.
@@ -15,8 +14,7 @@
 CFLAGS='-DUSE_MINIINC1 -ansi -pedantic -nostdinc -m32 -s -U_FORTIFY_SOURCE -fno-stack-protector -fno-ident -fomit-frame-pointer -mpreferred-stack-boundary=2 -falign-functions=1 -falign-jumps=1 -falign-loops=1 -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-unroll-loops -fmerge-all-constants -Os -W -Wall -Wextra -Wsystem-headers -Werror=implicit -Werror=implicit-int -Werror=implicit-function-declaration --sysroot minidiet -isystem minidiet -static-libgcc -DUSE_MINIINC2'
 # Not needed: ld -z norelro --build-id=none
 LDFLAGS1='-nostdlib -m elf_i386 -static -s minidiet/start.o'
-LDFLAGS2='minidiet/lib1.a minidiet/libgcc1.a'
-STRIP='strip -s --strip-unneeded --remove-section=.note.gnu.gold-version --remove-section=.comment --remove-section=.note --remove-section=.note.gnu.build-id --remove-section=.note.ABI-tag --remove-section=.eh_frame --remove-section=.eh_frame_ptr --remove-section=.jcr --remove-section=.got.plt'
+LDFLAGS2='minidiet/lib1.a minidiet/libgcc1.a -T minidiet/xtiny.scr'
 
 # strip -g --remove-section=.eh_frame --remove-section=.comment *.o
 # perl -ne 'print"$1\n" if m@^(?:free|__errno_location|mremap|__unified_syscall|errno|mmap|munmap|exit|stackgap|__libc_initary|strstr|getenv|__guard|memcmp|environ|close|read|open|close|set_thread_area|fputs|stdout|utimes|gettimeofday|fchmod|fileno|fflush|fwrite_unlocked|lstat|symlink|unlink|strcmp|ferror|ftell|fseek|fwrite|fputc_unlocked|isatty|__libc_write|lseek|__stdin_is_tty|__stack_chk_fail|ioctl|fread|fclose|fopen|chmod|mkdir|umask|__write2|fgetc_unlocked|__stdio_init_file|__stdio_parse_mode|__stdio_init_file_nothreads|fstat|feof_unlocked) (\S+)$@ ' smap | sort | uniq | tee olist && ar cr ../minidiet/lib1.a `cat olist`
@@ -27,12 +25,10 @@ test -f minidiet/lib1.a
 test -f minidiet/libgcc1.a
 test -f minidiet/start.o
 test -f minidiet/miniinc1.h
+test -f minidiet/xtiny.scr
 
 gcc $CFLAGS -c all.c
 ld -o tiny7zx $LDFLAGS1 all.o $LDFLAGS2
-ls -l tiny7zx
-$STRIP tiny7zx  # Makes tiny7zx.unc smaller.
-./sstrip tiny7zx  # Makes both the tiny7xz and tiny7zx.unc smaller.
 cp -a tiny7zx tiny7zx.unc
 ./upx.pts -q -q --ultra-brute tiny7zx
 ls -ld tiny7zx tiny7zx.unc
